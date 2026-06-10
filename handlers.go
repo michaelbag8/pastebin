acc0e414d8bc41819c7f6d97b1134c6da64348e8
michaelbag8 <michaelbag8@gmail.com>
2026-06-10 22:28:23 +0100

    health handler created

--- handlers.go
+++ handlers.go
@@ -1,10 +1,17 @@
 package main
 
-
-import(
+import (
+	"encoding/json"
 	"net/http"
 )
 
-func pasteHandler(w http.ResponseWriter, r *http.Request){
-
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	err:= json.NewEncoder(w).Encode(map[string]string{
+		"status":"ok",
+	})
+	if err!=nil{
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
 }
