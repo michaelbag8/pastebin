3a32dc20d433713ca81d64032e4fe4113ed2f5b6
michaelbag8 <michaelbag8@gmail.com>
2026-06-10 22:45:57 +0100

    createhandler added

--- handlers.go
+++ handlers.go
@@ -7,11 +7,36 @@ import (
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	err:= json.NewEncoder(w).Encode(map[string]string{
-		"status":"ok",
+	err := json.NewEncoder(w).Encode(map[string]string{
+		"status": "ok",
 	})
-	if err!=nil{
+	if err != nil {
 		http.Error(w, "internal server error", http.StatusInternalServerError)
 		return
 	}
+}
+
+func createPasteHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var req struct{
+		Content string `json:"content"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err!=nil{
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	if req.Content == ""{
+		http.Error(w, "Content is empty", http.StatusBadRequest)
+		return
+	}
+
+	shortCode := generateID(6)
+	if Paste.IsPublic == false{
+		privateKey := generateID(16)
+	}else{
+		publickey := shortCode
+	}
+
 }
