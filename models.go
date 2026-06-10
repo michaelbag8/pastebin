409b7d00657b74ad0c89ad6ef081342b9363ff7d
michaelbag8 <michaelbag8@gmail.com>
2026-06-10 22:29:34 +0100

    struct model created

--- models.go
+++ models.go
@@ -1,15 +1,28 @@
 package main
 
-import "time"
+import (
+	"time"
+	"math/rand"
+)
 
 type Paste struct {
-    Title      string     `json:"title"`
-    Content    string     `json:"content"`
-    Language   string     `json:"language"`
-    Views      int        `json:"views"`
-    CreatedAt  time.Time  `json:"created_at"`
-    ExpiresAt  *time.Time `json:"expires_at"`
-    ShortID    string     `json:"short_id"`
-    PrivateKey string 	  `json:"private_key,omitempty"`
-    IsPublic   bool       `json:"is_public"`
+	Title      string     `json:"title"`
+	Content    string     `json:"content"`
+	Language   string     `json:"language"`
+	Views      int        `json:"views"`
+	CreatedAt  time.Time  `json:"created_at"`
+	ExpiresAt  *time.Time `json:"expires_at"`
+	ShortID    string     `json:"short_id"`
+	PrivateKey string     `json:"private_key,omitempty"`
+	IsPublic   bool       `json:"is_public"`
+}
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func generateID(length int) string {
+	code := make([]byte, length)
+	for i := range code {
+		code[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(code)
 }
