c66ce64c29c5da1b1df41d893e4a0619d64bf90c
michaelbag8 <michaelbag8@gmail.com>
2026-06-10 22:27:43 +0100

    mutex fix

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
-
-import(
+import (
+	"log"
 	"net/http"
+	"sync"
+	"fmt"
 )
 
+var (
+	pasteStore = map[string]Paste{}
+	mu         sync.RWMutex
+)
+
+
 func main() {
-	http.HandleFunc("POST   /pastes     ", pasteHandler)     
-http.HandleFunc("GET    /pastes     ", pasteHandler)     
-http.HandleFunc("GET    /pastes/{id}", pasteHandler)     
-http.HandleFunc("DELETE /pastes/{id}", pasteHandler) 
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /health", healthHandler)
+	fmt.Println("Server starting on port 8080...")
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
+
+
+	mux.HandleFunc("POST   /pastes     ", pasteHandler)
+	mux.HandleFunc("GET    /pastes     ", pasteHandler)
+	mux.HandleFunc("GET    /pastes/{id}", pasteHandler)
+	mux.HandleFunc("DELETE /pastes/{id}", pasteHandler)
